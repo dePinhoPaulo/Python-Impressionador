f6521c85ae1d12afc81b991fa61150a6839bae1c
dePinhoPaulo <kiko.sjs@gmail.com>
2026-01-29 21:37:17 -0300

    Update 25. Integração Python com Arquivos txt e PDF
    Anotações e Prática. Aulão 08. Parte 2.

--- 03. Intermediário/25. Integração Python com Arquivos txt e PDF/Leitura de XML e Notas Fiscais com Python/projeto.py
+++ 03. Intermediário/25. Integração Python com Arquivos txt e PDF/Leitura de XML e Notas Fiscais com Python/projeto.py
@@ -1,7 +1,42 @@
 import xmltodict
 import pprint
 
-with open('NFs Finais/DANFEBrota.xml', 'rb') as arquivo:
+with open('NFs Finais/DANFENespresso.xml', 'rb') as arquivo:
     documento = xmltodict.parse(arquivo)
 
 pprint.pprint(documento)
+
+pprint.pprint(documento['nfeProc']['NFe']['infNFe']['dest']['CPF'])
+
+for item in documento['nfeProc']['NFe']:
+    print(item)
+
+# valor_total, produtos/servicos (valores), cnpj_vendeu, nome_vendeu, cpf/cnpj_comprou,  nome_comprou
+
+dic_nf = documento['nfeProc']['NFe']['infNFe']
+
+valor_total = dic_nf['total']['ICMSTot']['vNF']
+cnpj_vendeu = dic_nf['emit']['CNPJ']
+nome_vendeu = dic_nf['emit']['xNome']
+cpf_comprou = dic_nf['dest']['CPF']
+nome_comprou = dic_nf['dest']['xNome']
+
+lista_produtos = []
+produtos = dic_nf['det']
+for produto in produtos:
+    valor_produto = produto['prod']['vProd']
+    nome_produto = produto['prod']['xProd']
+    lista_produtos.append((nome_produto, valor_produto))
+
+resposta = {
+    'Valor Total': valor_total,
+    'CNPJ Vendedor': cnpj_vendeu,
+    'Razão Social': nome_vendeu,
+    'CPF Comprador': cpf_comprou,
+    'Nome Comprador': nome_comprou,
+    'Lista de Produtos': lista_produtos
+}
+
+pprint.pprint(resposta)
+
+# Inserir informações em um Banco de dados.
