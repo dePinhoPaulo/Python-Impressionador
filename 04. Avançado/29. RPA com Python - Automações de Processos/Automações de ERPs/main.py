3fd9f889a592f8c70f643b363da282cca11475b3
dePinhoPaulo <kiko.sjs@gmail.com>
2026-03-12 21:45:16 -0300

    Update 29. RPA com Python - Automações de Processos
    Anotações e Prática. Aulão 02. Parte 3.

--- 04. Avançado/29. RPA com Python - Automações de Processos/Automações de ERPs/main.py
+++ 04. Avançado/29. RPA com Python - Automações de Processos/Automações de ERPs/main.py
@@ -9,6 +9,8 @@
 import pyautogui
 import subprocess
 import time
+import pandas as pd
+import pyperclip
 
 pyautogui.FAILSAFE = True #interrompe o programa a qualquer momento em caso de falaha, mexendo o mouse pa extremidade
 
@@ -21,71 +23,100 @@ def encontrar_imagem(imagem):
 def direita(posicaoes_imagem):
     return posicaoes_imagem[0] + posicaoes_imagem[2], posicaoes_imagem[1] + posicaoes_imagem[3]/2 #Localiza dirieta da imgem
 
+# Passo 7 - Mostrar cacteres com Acento
+def escrever_texto(texto):
+    pyperclip.copy(texto)
+    pyautogui.hotkey('ctrl', 'v')
+
 # Passo 1 - Abrir Fakturama
 subprocess.Popen([r'C:\Program Files\Fakturama2\Fakturama.exe'])
 
 encontrou = encontrar_imagem('arquivo.png') # Recnhecimento de imagem
 # Fakturama aberto!
 
-# Passo 2 - Clicar no menu
-# Esperar menu carregar
-encontrou = encontrar_imagem('menu-new.png')
-# Clicar no menu
-pyautogui.click(pyautogui.center(encontrou))
+# Passo 6 - Integar preenchimento com planilha no Excel
+df_produtos = pd.read_excel('Produtos.xlsx')
 
-# Esperar menu new product carregar
-encontrou = encontrar_imagem('menu-new-product.png')
-# Clicar no menu
-pyautogui.click(pyautogui.center(encontrou))
+for linha in df_produtos.index:
+    nome = df_produtos.loc[linha, 'Nome']
+    id = df_produtos.loc[linha, 'ID']
+    categoria = df_produtos.loc[linha, 'Categoria']
+    gtin = df_produtos.loc[linha, 'Categoria']
+    supplier = df_produtos.loc[linha, 'Categoria']
+    descricao = df_produtos.loc[linha, 'Descrição']
+    imagem = df_produtos.loc[linha, 'Imgem']
+    preco = df_produtos.loc[linha, 'Preço']
+    custo = df_produtos.loc[linha, 'Custo']
+    estoque = df_produtos.loc[linha, 'Estoque']
 
-# Passo 3 - Preencher todos campos
-# Item number
-encontrou = encontrar_imagem('item-number.png')
-# Clicar na dirieta da imgem
-pyautogui.click(direita(encontrou))
-# Digitar
-pyautogui.write("2")
 
-# Name
-pyautogui.press('tab') #Pular para proximo campo
-pyautogui.write("Computador")
+    # Passo 2 - Clicar no menu
+    # Esperar menu carregar
+    encontrou = encontrar_imagem('menu-new.png')
+    # Clicar no menu
+    pyautogui.click(pyautogui.center(encontrou))
 
-# Category
-pyautogui.press('tab') 
-pyautogui.write("Tech")
+    # Esperar menu new product carregar
+    encontrou = encontrar_imagem('menu-new-product.png')
+    # Clicar no menu
+    pyautogui.click(pyautogui.center(encontrou))
 
-# GTIN
-pyautogui.press('tab') 
-pyautogui.write("12354")
+    # Passo 3 - Preencher todos campos
+    # Item number
+    encontrou = encontrar_imagem('item-number.png')
+    # Clicar na dirieta da imgem
+    pyautogui.click(direita(encontrou))
+    # Digitar
+    pyautogui.write(str(id))
 
-# Supplier code
-pyautogui.press('tab') 
-pyautogui.write("6584")
+    # Name
+    pyautogui.press('tab') #Pular para proximo campo
+    pyautogui.write(str(nome))
 
-# Description
-pyautogui.press('tab') 
-pyautogui.write("PC Gamer")
+    # Category
+    pyautogui.press('tab') 
+    pyautogui.write(str(categoria))
 
-# Price
-pyautogui.press('tab') 
-pyautogui.write('1250,00') 
+    # GTIN
+    pyautogui.press('tab') 
+    pyautogui.write(str(gtin))
 
-# Cost Price
-encontrou = encontrar_imagem('coste-price.png')
-pyautogui.click(direita(encontrou))
-pyautogui.write('550,00')
+    # Supplier code
+    pyautogui.press('tab') 
+    pyautogui.write(str(supplier))
 
-# Stock
-encontrou = encontrar_imagem('coste-price.png')
-pyautogui.click(direita(encontrou))
-pyautogui.write('5,00')
+    # Description
+    pyautogui.press('tab') 
+    pyautogui.write(str(descricao))
 
-# Passo 4 - Upload imagem
-encontrou = encontrar_imagem('select-picture.png')
-pyautogui.click(pyautogui.center(encontrou))
+    # Price
+    pyautogui.press('tab') 
+    preco_texto = f'{preco:.2f}'.replace('.', ',')
+    pyautogui.write(str(preco_texto)) 
 
-# Navegação Windows
-encontrou = encontrar_imagem('select-picture.png')
-pyautogui.click(pyautogui.center(encontrou))
-pyautogui.write(r'C:\Users\Kiko\Documents\Projetos Programação\Python-Impressionador\04. Avançado\29. RPA com Python - Automações de Processos\Automações de ERPs\notebook.jpg')
-pyautogui.press('enter') 
+    # Cost Price
+    encontrou = encontrar_imagem('coste-price.png')
+    pyautogui.click(direita(encontrou))
+    custo_texto = f'{preco:.2f}'.replace('.', ',')
+    pyautogui.write(str(custo_texto))
+
+    # Stock
+    encontrou = encontrar_imagem('coste-price.png')
+    pyautogui.click(direita(encontrou))
+    estoque_texto = f'{preco:.2f}'.replace('.', ',')
+    pyautogui.write(str(estoque_texto))
+
+    # Passo 4 - Upload imagem
+    encontrou = encontrar_imagem('select-picture.png')
+    pyautogui.click(pyautogui.center(encontrou))
+
+    # Navegação Windows
+    encontrou = encontrar_imagem('nome-arquivo.png')
+    pyautogui.click(pyautogui.center(encontrou))
+    pyautogui.write(rf'C:\Users\Kiko\Documents\Projetos Programação\Python-Impressionador\04. Avançado\29. RPA com Python - Automações de Processos\Automações de ERPs\{str(imagem)}')
+    pyautogui.press('enter') 
+
+    # Passo 5 - Salvar Cadastro
+    # Salvar
+    encontrou = encontrar_imagem('salvar.png')
+    pyautogui.click(pyautogui.center(encontrou))
