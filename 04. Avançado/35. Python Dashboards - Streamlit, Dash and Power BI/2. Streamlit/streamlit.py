c5117accc8ebe3e70d50f6e25a68b91208543c8f
dePinhoPaulo <kiko.sjs@gmail.com>
2026-05-06 13:39:02 -0300

    Update 35. Python Dashboards - Streamlit, Dash and Power BI
    Anotações e Prática. Aula 02.

--- 04. Avançado/35. Python Dashboards - Streamlit, Dash and Power BI/2. Streamlit/streamlit.py
+++ 04. Avançado/35. Python Dashboards - Streamlit, Dash and Power BI/2. Streamlit/streamlit.py
@@ -1,27 +1,23 @@
-# app resultado de ações
+# dark mode
 import streamlit as st
 import yfinance as yf
 import pandas as pd
 
 st.write("""
 # App Preço de Ações
-O gráfico abaixo representa a evolução do preço das ações do Itaú (ITUB4) ao longo dos anos
+O gráfico abaixo representa a evolução do preço das ações brasileiras ao longo dos anos
 """)
 
-def carregar_dados(empresa):
-    dados_acao = yf.Ticker(empresa)
-    precos_acao = dados_acao.history(period='1d', start='2000-01-01', end='2024-07-01')
-    # precos_acao = pd.read_csv("BaseItau.csv")
-    # precos_acao["Date"] = pd.to_datetime(precos_acao["Date"])
-    # precos_acao = precos_acao.set_index("Date")
-    precos_acao = precos_acao[["Close"]]
+@st.cache_data
+def carregar_dados(empresas):
+    dados_acao = yf.Tickers(empresas)
+    precos_acao = dados_acao.history(period='1d', start='2015-01-01', end='2024-07-01')
+    precos_acao = precos_acao["Close"]
     return precos_acao
 
-dados  = carregar_dados("ITUB4.SA")
-print(dados)
+dados  = carregar_dados("ITUB4.SA BBAS3.SA VALE3.SA ABEV3.SA MGLU3.SA PETR4.SA GGBR4.SA")
+
+lista_acoes = st.multiselect("Escolha as ações para exibir no gráfico", dados.columns)
+print(lista_acoes)
+dados = dados[lista_acoes]
 grafico = st.line_chart(dados)
-
-st.write("""
-# Fim do app
-""")
-
