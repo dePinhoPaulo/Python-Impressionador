19a190fdadf0db458b62da52e6a062c8c386e341
dePinhoPaulo <kiko.sjs@gmail.com>
2026-05-07 14:14:12 -0300

    Update 35. Python Dashboards - Streamlit, Dash and Power BI
    Anotações e Prática. Aula 03.

--- 04. Avançado/35. Python Dashboards - Streamlit, Dash and Power BI/2. Streamlit/streamlit.py
+++ 04. Avançado/35. Python Dashboards - Streamlit, Dash and Power BI/2. Streamlit/streamlit.py
@@ -1,7 +1,7 @@
-# dark mode
 import streamlit as st
 import yfinance as yf
 import pandas as pd
+from datetime import timedelta
 
 st.write("""
 # App Preço de Ações
@@ -17,7 +17,21 @@ def carregar_dados(empresas):
 
 dados  = carregar_dados("ITUB4.SA BBAS3.SA VALE3.SA ABEV3.SA MGLU3.SA PETR4.SA GGBR4.SA")
 
-lista_acoes = st.multiselect("Escolha as ações para exibir no gráfico", dados.columns)
-print(lista_acoes)
-dados = dados[lista_acoes]
-grafico = st.line_chart(dados)
+barra_lateral = st.sidebar.header("Filtros")
+lista_acoes = st.sidebar.multiselect("Escolha as ações para exibir no gráfico", dados.columns)
+data_inicial = dados.index.min().to_pydatetime()
+data_final = dados.index.max().to_pydatetime()
+intervalo_datas = st.sidebar.slider("Selecione o período", 
+                                    min_value=data_inicial, 
+                                    max_value=data_final, value=(data_inicial, data_final), step=timedelta(days=1))
+
+# filtrar data
+dados = dados.loc[intervalo_datas[0]:intervalo_datas[1]]
+# filtrar ações
+if lista_acoes:
+    dados = dados[lista_acoes]
+    if len(lista_acoes)==1:
+        dados = dados.rename(columns={lista_acoes[0]: "Close"})
+    
+
+st.line_chart(dados)
