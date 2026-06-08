f41753fde57fc4a7518f4fb28e6b7880463d80cf
dePinhoPaulo <kiko.sjs@gmail.com>
2026-06-08 14:06:52 -0300

    Update 35. Python Dashboards - Streamlit, Dash and Power BI
    Anotações e Prática. Aulas da 10 a 11.

--- 04. Avançado/35. Python Dashboards - Streamlit, Dash and Power BI/3. Streamlit_2/homepage.py
+++ 04. Avançado/35. Python Dashboards - Streamlit, Dash and Power BI/3. Streamlit_2/homepage.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.title("Homepage")
+
+# containers
+# columns
+
+coluna_esquerda, coluna_direita = st.columns([1, 1.5])
+
+coluna_esquerda.title("Hash&Co")
+coluna_esquerda.write("#### Bem vindo, Fulano") # markdown
+botao_dashboards = coluna_esquerda.button("Dashboard Projetos")
+botao_indicadores = coluna_esquerda.button("Principais Indicadores")
+
+if botao_dashboards:
+    st.switch_page("dashboard.py")
+if botao_indicadores:
+    st.switch_page("indicadores.py")
+
+container = coluna_direita.container(border=True)
+container.image("imagens/time-comunidade.webp")
