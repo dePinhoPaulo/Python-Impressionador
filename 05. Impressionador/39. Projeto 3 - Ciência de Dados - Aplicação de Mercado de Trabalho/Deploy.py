d09ec0ce0e5be6d9b60ded65450878143fcfd491
dePinhoPaulo <kiko.sjs@gmail.com>
2026-08-11 14:46:21 -0300

    Update 39. Projeto 3 - Ciência de Dados - Aplicação de Mercado de Trabalho
    Anotações e Prática. Aulas da 58 a 59.

--- 05. Impressionador/39. Projeto 3 - Ciência de Dados - Aplicação de Mercado de Trabalho/Deploy.py
+++ 05. Impressionador/39. Projeto 3 - Ciência de Dados - Aplicação de Mercado de Trabalho/Deploy.py
@@ -15,13 +15,30 @@ x_listas = {'property_type': ['Apartment', 'Bed and breakfast', 'Condominium', '
             'cancelation_policy': ['flexible', 'moderate', 'strict', 'strict_14_with_grace_period']
             }
 
+dicionario= {}
+for item in x_listas:
+    for valor in x_listas[item]:
+        dicionario[f'{item}_{valor}'] = 0
+
+
 for item in x_numericos:
-    valor  = st.number_input(f'{item}')
+    if item == 'latitude' or item == 'longitude':
+        valor = st.number_input(f'{item}', step=0.00001, value=0.0, format="%.5f")
+    elif item == 'extra_people':
+        valor = st.number_input(f'{item}', step=0.01, value=0.0)
+    else:
+        valor = st.number_input(f'{item}', step=1, value=0)
+    x_numericos[item] = valor
 
 for item in x_tf:
     valor  = st.selectbox(f'{item}', ('Sim', 'Não'))
+    if valor == 'Sim':
+        x_tf[item] = 1
+    else:
+        x_tf[item] = 0
     
 for item in x_listas:
     valor  = st.selectbox(f'{item}', x_listas[item])
+    dicionario[f'{item}_{valor}'] = 1
 
 botao = st.button('Prever Valor do Imovel')
