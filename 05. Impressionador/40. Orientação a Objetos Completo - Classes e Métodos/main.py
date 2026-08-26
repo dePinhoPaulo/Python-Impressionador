8c990e555d28b14f0c074a1cb8f4fd830f1394e0
dePinhoPaulo <kiko.sjs@gmail.com>
2026-08-26 14:13:10 -0300

    Update 40. Orientação a Objetos Completo - Classes e Métodos
    Anotações e Prática. Aulas da 4 a 5.

--- 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/main.py
+++ 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/main.py
@@ -1,1 +1,22 @@
-a = 13 #TODO ASDA
+nome = 'PAULO'
+
+print(type(nome))
+print(nome.capitalize())
+
+class TV():
+    cor = 'preto'
+    tamanho = 55
+    canal = 10
+    volume = 50
+
+    def mudar_canal(self):
+        pass
+
+    def mudar_voluma(self):
+        pass
+
+    def ligar_desligar(self):
+        pass
+
+TV.mudar_canal()
+TV.ligar_desligar()
