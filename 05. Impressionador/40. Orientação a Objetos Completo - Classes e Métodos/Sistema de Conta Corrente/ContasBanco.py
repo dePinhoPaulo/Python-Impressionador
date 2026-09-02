b9c243b9f7dd9aa753d620ed31f6a562613ad503
dePinhoPaulo <kiko.sjs@gmail.com>
2026-09-02 14:30:00 -0300

    Update 40. Orientação a Objetos Completo - Classes e Métodos
    Anotações e Prática. Aulas da 16 a 18.

--- 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/ContasBanco.py
+++ 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/ContasBanco.py
@@ -1,16 +1,29 @@
+from datetime import datetime
+import pytz
+
 class ContaCorrente:
 
-    def __init__(self, nome, cpf):
+    @staticmethod
+    def _data_hora():
+        fuso_BR = pytz.timezone('Brazil/East')
+        horario_BR = datetime.now(fuso_BR)
+        return horario_BR.strftime("%d/%m/%y %H:%M:%S")
+    
+    def __init__(self, nome, cpf, agencia, num_conta):
         self.nome = nome
         self.cpf = cpf
         self.saldo = 0
         self.limite = None
+        self.agencia = agencia
+        self.num_conta = num_conta
+        self.transacoes = []
 
     def consultar_saldo(self):
         print("Seu saldo é de: R${:,.2f}".format(self.saldo))
 
     def depositar(self, valor):
         self.saldo += valor
+        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
 
     def _limite_conta(self):
         self.limite = -100
@@ -22,12 +35,19 @@ class ContaCorrente:
             self.consultar_saldo()
         else:
             self.saldo -= valor
+            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
 
     def consultar_limite_cheque_especial(self):
         print("Seu limite de cheque especial é de: R${:,.2f}".format(self._limite_conta()))
 
+    def consultar_transacoes(self):
+        print("Histórico de Transações: ")
+        print("Valor, Saldo, Data e Hora")
+        for transacao in self.transacoes:
+            print(transacao)
+
 #programa
-conta_paulo = ContaCorrente('Paulo', '123.456.789.00')
+conta_paulo = ContaCorrente('Paulo', '123.456.789.00', 1, 21448)
 
 print(conta_paulo.cpf)
 print(conta_paulo.saldo)
@@ -38,3 +58,7 @@ conta_paulo.sacar(60)
 
 conta_paulo.consultar_saldo()
 conta_paulo.consultar_limite_cheque_especial()
+
+print("-" * 25)
+
+print(conta_paulo.consultar_transacoes())
