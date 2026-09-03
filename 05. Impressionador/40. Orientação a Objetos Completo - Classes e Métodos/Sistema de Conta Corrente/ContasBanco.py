4beb15b5cf77b056056b2221060960e472427099
dePinhoPaulo <kiko.sjs@gmail.com>
2026-09-03 14:56:59 -0300

    Update 40. Orientação a Objetos Completo - Classes e Métodos
    Anotações e Prática. Aulas da 19 a 21.

--- 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/ContasBanco.py
+++ 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/ContasBanco.py
@@ -2,7 +2,18 @@ from datetime import datetime
 import pytz
 
 class ContaCorrente:
+    """
+    Cria um objeto conta corrente para gerenciar as contas dos clientes.
 
+    Atributos:
+        _nome (str): Nome do cliente 
+        _cpf (str): CPF do cliente. Deve ser inserido com pontos e traço
+        _saldo (float): Saldo atual do cliente
+        _limite (float): _saldo negativo maximo parmitido para o cliente
+        agencia (int): numero de cadastro da agencia
+        num_conta (int): numero de cadastro da conta
+        _transacoes (tuple): historioco de transações da conta
+    """
     @staticmethod
     def _data_hora():
         fuso_BR = pytz.timezone('Brazil/East')
@@ -10,55 +21,75 @@ class ContaCorrente:
         return horario_BR.strftime("%d/%m/%y %H:%M:%S")
     
     def __init__(self, nome, cpf, agencia, num_conta):
-        self.nome = nome
-        self.cpf = cpf
-        self.saldo = 0
-        self.limite = None
+        self._nome = nome
+        self._cpf = cpf
+        self._saldo = 0
+        self._limite = None
         self.agencia = agencia
         self.num_conta = num_conta
-        self.transacoes = []
+        self._transacoes = []
 
-    def consultar_saldo(self):
-        print("Seu saldo é de: R${:,.2f}".format(self.saldo))
+    def consultar__saldo(self):
+        """
+        Exibe o _saldo atual da conta corrente.
+        """
+        print("Seu _saldo é de: R${:,.2f}".format(self._saldo))
 
     def depositar(self, valor):
-        self.saldo += valor
-        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
+        self._saldo += valor
+        self._transacoes.append((valor, self._saldo, ContaCorrente._data_hora()))
 
-    def _limite_conta(self):
-        self.limite = -100
-        return self.limite
+    def limite_conta(self):
+        self._limite = -100
+        return self._limite
     
     def sacar(self, valor):
-        if self.saldo - valor < self._limite_conta():
-            print("Você não tem saldo sulficiente para sacar esse valor!")
-            self.consultar_saldo()
+        if self._saldo - valor < self.__limite_conta():
+            print("Você não tem _saldo sulficiente para sacar esse valor!")
+            self.consultar__saldo()
         else:
-            self.saldo -= valor
-            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
+            self._saldo -= valor
+            self._transacoes.append((-valor, self._saldo, ContaCorrente._data_hora()))
 
     def consultar_limite_cheque_especial(self):
-        print("Seu limite de cheque especial é de: R${:,.2f}".format(self._limite_conta()))
+        print("Seu _limite de cheque especial é de: R${:,.2f}".format(self.__limite_conta()))
 
-    def consultar_transacoes(self):
+    def consultar__transacoes(self):
         print("Histórico de Transações: ")
         print("Valor, Saldo, Data e Hora")
-        for transacao in self.transacoes:
+        for transacao in self._transacoes:
             print(transacao)
+
+    def transferir(self, valor, conta_destino):
+        self._saldo -= valor
+        self._transacoes.append((-valor, self._saldo, ContaCorrente._data_hora()))
+        conta_destino._saldo += valor
+        conta_destino._transacoes.append((valor, conta_destino._saldo, ContaCorrente._data_hora()))
 
 #programa
 conta_paulo = ContaCorrente('Paulo', '123.456.789.00', 1, 21448)
 
-print(conta_paulo.cpf)
-print(conta_paulo.saldo)
+print(conta_paulo._cpf)
+print(conta_paulo._saldo)
 
 conta_paulo.depositar(100)
 conta_paulo.sacar(54.5)
 conta_paulo.sacar(60)
 
-conta_paulo.consultar_saldo()
-conta_paulo.consultar_limite_cheque_especial()
+conta_paulo.consultar__saldo()
+conta_paulo.consultar__limite_cheque_especial()
 
 print("-" * 25)
 
-print(conta_paulo.consultar_transacoes())
+print(conta_paulo.consultar__transacoes())
+
+print("-" * 25)
+
+conta_maePaulo = ContaCorrente('Naia', '000.456.000.00', 1, 21450)
+
+conta_paulo.transferir(50, conta_maePaulo)
+
+conta_paulo.consultar__transacoes()
+conta_maePaulo.consultar__transacoes()
+
+help(ContaCorrente)
