329ed7aca8bbb0eb05d9208a74184170741aca7a
dePinhoPaulo <kiko.sjs@gmail.com>
2026-09-08 14:19:21 -0300

    Update 40. Orientação a Objetos Completo - Classes e Métodos
    Anotações e Prática. Aulas da 28 a 30.

--- 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/Agencia.py
+++ 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/Agencia.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Agencia:
 
     def __init__(self, telefone, cnpj, numero):
@@ -23,15 +25,49 @@ class Agencia:
     def adicionar_cliente(self, nome, cpf, patrimonio):
         self.clientes.append((nome, cpf, patrimonio))
 
-agencia_1 = Agencia(32671388, 12345678901, 1000)
+# agencia online
+class AgenciaVirtual(Agencia):
 
-agencia_1.caixa = 20000
+    def __init__(self, site, telefone, cnpj):
+        super().__init__(telefone, cnpj, 1000)
+        self.site = site
+        self.caixa = 1000000
+
+# agencia comum
+class AgenciaComum(Agencia):
+
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone, cnpj, numero=randint(1001, 9999))
+        self.caixa = 1000000
+
+# agencia premium
+class AgenciaPremium(Agencia):
+
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone, cnpj, numero=randint(1001, 9999))
+        self.caixa = 10000000
+
+
+agencia_1 = AgenciaComum(32671388, 12345678901)
+
+# agencia_1.caixa = 20000
 agencia_1.verificar_caixa()
+print(agencia_1.numero)
 
-agencia_1.emprestar_dinheiro(10000, 10012345678, 0.02)
+# agencia_1.emprestar_dinheiro(10000, 10012345678, 0.02)
 
-print(agencia_1.emprestimos)
+# print(agencia_1.emprestimos)
 
-agencia_1.adicionar_cliente('Paulo', 10012345678, 1000000)
+# agencia_1.adicionar_cliente('Paulo', 10012345678, 1000000)
 
-print(agencia_1.clientes)
+# print(agencia_1.clientes)
+
+agencia_virtual_1 = AgenciaVirtual('www.agenciavirtual.com', 22224444, 123456789858)
+agencia_virtual_1.verificar_caixa()
+print(agencia_virtual_1.site)
+
+agencia_premium_1 = AgenciaPremium(3267999, 12345999991)
+
+# agencia_1.caixa = 20000
+agencia_premium_1.verificar_caixa()
+print(agencia_premium_1.numero)
