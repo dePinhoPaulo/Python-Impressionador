dbe258ac3ebe5944259ae303080ef530b394740f
dePinhoPaulo <kiko.sjs@gmail.com>
2026-09-09 14:20:54 -0300

    Update 40. Orientação a Objetos Completo - Classes e Métodos
    Anotações e Prática. Aulas da 31 a 33.

--- 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/Agencia.py
+++ 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/Agencia.py
@@ -32,6 +32,15 @@ class AgenciaVirtual(Agencia):
         super().__init__(telefone, cnpj, 1000)
         self.site = site
         self.caixa = 1000000
+        self.caixa_paypal = 0
+
+    def depositar_paypal(self, valor):
+        self.caixa -= valor
+        self.caixa_paypal += valor
+ 
+    def sacar_paypal(self, valor):
+        self.caixa += valor
+        self.caixa_paypal -= valor
 
 # agencia comum
 class AgenciaComum(Agencia):
@@ -47,27 +56,42 @@ class AgenciaPremium(Agencia):
         super().__init__(telefone, cnpj, numero=randint(1001, 9999))
         self.caixa = 10000000
 
+    def adicionar_cliente(self, nome, cpf, patrimonio):
+        if patrimonio > 1000000:
+            super().adicionar_cliente(nome, cpf, patrimonio)
+        else:
+            print("Cliente não tem patrimonio minimo para entrar na Agencia Premium!")
 
-agencia_1 = AgenciaComum(32671388, 12345678901)
 
-# agencia_1.caixa = 20000
-agencia_1.verificar_caixa()
-print(agencia_1.numero)
+if __name__ == '__main__':
+    agencia_1 = AgenciaComum(32671388, 12345678901)
 
-# agencia_1.emprestar_dinheiro(10000, 10012345678, 0.02)
+    # agencia_1.caixa = 20000
+    agencia_1.verificar_caixa()
+    print(agencia_1.numero)
 
-# print(agencia_1.emprestimos)
+    # agencia_1.emprestar_dinheiro(10000, 10012345678, 0.02)
 
-# agencia_1.adicionar_cliente('Paulo', 10012345678, 1000000)
+    # print(agencia_1.emprestimos)
 
-# print(agencia_1.clientes)
+    # agencia_1.adicionar_cliente('Paulo', 10012345678, 1000000)
 
-agencia_virtual_1 = AgenciaVirtual('www.agenciavirtual.com', 22224444, 123456789858)
-agencia_virtual_1.verificar_caixa()
-print(agencia_virtual_1.site)
+    # print(agencia_1.clientes)
 
-agencia_premium_1 = AgenciaPremium(3267999, 12345999991)
+    agencia_virtual_1 = AgenciaVirtual('www.agenciavirtual.com', 22224444, 123456789858)
+    agencia_virtual_1.verificar_caixa()
+    print(agencia_virtual_1.site)
 
-# agencia_1.caixa = 20000
-agencia_premium_1.verificar_caixa()
-print(agencia_premium_1.numero)
+    agencia_premium_1 = AgenciaPremium(3267999, 12345999991)
+
+    # agencia_1.caixa = 20000
+    agencia_premium_1.verificar_caixa()
+    print(agencia_premium_1.numero)
+
+    agencia_virtual_1.depositar_paypal(20000)
+    print(agencia_virtual_1.caixa)
+    print(agencia_virtual_1.caixa_paypal)
+
+    agencia_premium_1.adicionar_cliente('Paulo', 12345678910, 50000000)
+    agencia_premium_1.adicionar_cliente('irmao_Paulo', 12345678910, 50000)
+    print(agencia_premium_1.clientes)
--- 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/main.py
+++ 05. Impressionador/40. Orientação a Objetos Completo - Classes e Métodos/Sistema de Conta Corrente/main.py
@@ -1,4 +1,5 @@
 from ContasBanco import ContaCorrente, CartaoCredito
+from Agencia import AgenciaComum, AgenciaPremium, AgenciaVirtual
 
 # Programa
 conta_paulo = ContaCorrente('Paulo', '123.456.789.00', 1, 21448)
@@ -42,3 +43,36 @@ print(cartao_paulo.senha)
 
 print(conta_paulo.__dict__)
 print(cartao_paulo.__dict__)
+
+
+agencia_1 = AgenciaComum(32671388, 12345678901)
+
+# agencia_1.caixa = 20000
+agencia_1.verificar_caixa()
+print(agencia_1.numero)
+
+# agencia_1.emprestar_dinheiro(10000, 10012345678, 0.02)
+
+# print(agencia_1.emprestimos)
+
+# agencia_1.adicionar_cliente('Paulo', 10012345678, 1000000)
+
+# print(agencia_1.clientes)
+
+agencia_virtual_1 = AgenciaVirtual('www.agenciavirtual.com', 22224444, 123456789858)
+agencia_virtual_1.verificar_caixa()
+print(agencia_virtual_1.site)
+
+agencia_premium_1 = AgenciaPremium(3267999, 12345999991)
+
+# agencia_1.caixa = 20000
+agencia_premium_1.verificar_caixa()
+print(agencia_premium_1.numero)
+
+agencia_virtual_1.depositar_paypal(20000)
+print(agencia_virtual_1.caixa)
+print(agencia_virtual_1.caixa_paypal)
+
+agencia_premium_1.adicionar_cliente('Paulo', 12345678910, 50000000)
+agencia_premium_1.adicionar_cliente('irmao_Paulo', 12345678910, 50000)
+print(agencia_premium_1.clientes)
